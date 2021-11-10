77153b1501477d0543b8b3034bdc2bbf716703c8
mauro-sousa <maurosousa94@gmail.com>
2021-11-10 21:37:41 +0200

    applicant in tab appear

--- assets/js/criteria.js
+++ assets/js/criteria.js
@@ -446,148 +446,147 @@ editModalForm.addEventListener('submit', e => {
 //     }
 // }
 
-
-
-    // Your web app's Firebase configuration
-    const firebaseConfig = {
-      apiKey: "AIzaSyBuoID-FVQH3stFpUJhQuKFFjRMQuPKnE0",
-      authDomain: "beauty-pageant-judging-system.firebaseapp.com",
-      projectId: "beauty-pageant-judging-system",
-      storageBucket: "beauty-pageant-judging-system.appspot.com",
-      messagingSenderId: "327388870081",
-      appId: "1:327388870081:web:7cc2a2ee298114ec4a6b40",
-      measurementId: "G-RB3GBY1QL6"
-    };
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-const db = firebase.firestore();
-
-const modalWrapperc = document.querySelector('.modal-wrapperc');
+const modalWrapperapp = document.querySelector('.modal-wrapperapp');
 // modal add
-const addModalc = document.querySelector('.add-modalc');
+const addModalapp = document.querySelector('.add-modalapp');
 // btnAddc.addEventListener('click', () => {
 //     console.log("works")
 //   });
-const addModalcForm2 = document.querySelector('.add-modalc .form');
+const addModalappForm3 = document.querySelector('.add-modalapp .form');
 
 // modal edit
-const editModalc = document.querySelector('.edit-modalc');
-const editModalcForm2 = document.querySelector('.edit-modalc .form');
+const editModalapp = document.querySelector('.edit-modalapp');
+const editModalappForm3 = document.querySelector('.edit-modalapp .form');
 
-const btnAddc = document.querySelector('.btn-addc');
+const btnAddapp = document.querySelector('.btn-addapp');
 
-const tableUsersc = document.querySelector('.table-usersc');
+const tableUsersapp = document.querySelector('.table-usersapp');
 
-let idc;
+let idapp;
 
 // Create element and render users
-const renderUserc = doc => {
+const renderUserapp = doc => {
   const tr = `
-    <tr data-idc='${doc.id}'>
-      <td>${doc.data().Name}</td>
-      <td>${doc.data().Rate}</td>
-      <td>${doc.data().Description}</td>
+    <tr data-idapp='${doc.id}'>
+      <td>${doc.data().firstName}</td>
+      <td>${doc.data().lastName}</td>
+      <td>${doc.data().email}</td>
+      <td>${doc.data().Nationality}</td>
+      <td>${doc.data().StudentNr}</td>
+      <td>${doc.data().ContestantNo}</td>
       <td>
-        <button class="btn btn-editc">Edit</button>
-        <button class="btn btn-deletec">Delete</button>
+        <button class="btn btn-editapp">Edit</button>
+        <button class="btn btn-deleteapp">Delete</button>
       </td>
     </tr>
   `;
-  tableUsersc.insertAdjacentHTML('beforeend', tr);
+  tableUsersapp.insertAdjacentHTML('beforeend', tr);
 
   // Click edit user
-  const btnEdit = document.querySelector(`[data-idc='${doc.id}'] .btn-editc`);
+  const btnEdit = document.querySelector(`[data-idapp='${doc.id}'] .btn-editapp`);
   btnEdit.addEventListener('click', () => {
-    editModalc.classList.add('modal-showc');
-    idc = doc.id;
-    editModalcForm2.Name.value = doc.data().Name;
-    editModalcForm2.Rate.value = doc.data().Rate;
-    editModalcForm2.Description.value = doc.data().Description;
+    editModalapp.classList.add('modal-showapp');
+    idapp = doc.id;
+    editModalappForm3.firstname.value = doc.data().firstname;
+    editModalappForm3.lastname.value = doc.data().lastname;
+    editModalappForm3.email.value = doc.data().email;
+    editModalappForm3.Nationality.value = doc.data().Nationality;
+    editModalappForm3.StudentNr.value = doc.data().StudentNr;
+    editModalappForm3.ContestantNo.value = doc.data().ContestantNo;
 
   });
 
   // Click delete user
-  const btnDelete = document.querySelector(`[data-idc='${doc.id}'] .btn-deletec`);
+  const btnDelete = document.querySelector(`[data-idapp='${doc.id}'] .btn-deleteapp`);
 
   btnDelete.addEventListener('click', () => {
     //add confirmation code
 
-    db.collection('Criteria').doc(`${doc.id}`).delete().then(() => {
+    db.collection('Applications').doc(`${doc.id}`).delete().then(() => {
       console.log('Document successfully deleted!');
-      alert('Criteria deleted successfully!')
+      alert('Application deleted successfully!')
     }).catch(err => {
       console.log('Error removing document', err);
-      alert('Error removing Criteria',err);
+      alert('Error removing Application',err);
 
     });
   });
 }
 
 // Click add user button
-btnAddc.addEventListener('click', () => {
-  addModalc.classList.add('modal-showc');
+btnAddapp.addEventListener('click', () => {
+  addModalapp.classList.add('modal-showapp');
 
-  addModalcForm2.Name.value = '';
-  addModalcForm2.Rate.value = '';
-  addModalcForm2.Description.value = '';
+  addModalappForm3.firstname.value = '';
+  addModalappForm3.lastname.value = '';
+  addModalappForm3.email.value = '';
+  addModalappForm3.Nationality.value = '';
+  addModalappForm3.StudentNr.value = '';
+  addModalappForm3.ContestantNo.value = '';
 });
 
-// User click anyware outside the modal
+// User click anywhere outside the modal
 window.addEventListener('click', e => {
-  if(e.target === addModalc) {
-    addModalc.classList.remove('modal-showc');
+  if(e.target === addModalapp) {
+    addModalapp.classList.remove('modal-showapp');
   }
-  if(e.target === editModalc) {
-    editModalc.classList.remove('modal-showc');
+  if(e.target === editModalapp) {
+    editModalapp.classList.remove('modal-showapp');
   }
 });
 
-// Get all users
-// db.collection('users').get().then(querySnapshot => {
+// // Get all users
+// db.collection('Applications').get().then(querySnapshot => {
 //   querySnapshot.forEach(doc => {
 //     renderUserc(doc);
 //   })
 // });
 
 // Real time listener
-db.collection('Criteria').onSnapshot(snapshot => {
+db.collection('Applications').onSnapshot(snapshot => {
   snapshot.docChanges().forEach(change => {
     if(change.type === 'added') {
-      renderUserc(change.doc);
+      renderUserapp(change.doc);
     }
     if(change.type === 'removed') {
-      let tr = document.querySelector(`[data-idc='${change.doc.id}']`);
+      let tr = document.querySelector(`[data-idapp='${change.doc.id}']`);
       let tbody = tr.parentElement;
-      tableUsersc.removeChild(tbody);
+      tableUsersapp.removeChild(tbody);
     }
     if(change.type === 'modified') {
-      let tr = document.querySelector(`[data-idc='${change.doc.id}']`);
+      let tr = document.querySelector(`[data-idapp='${change.doc.id}']`);
       let tbody = tr.parentElement;
-      tableUsersc.removeChild(tbody);
-      renderUserc(change.doc);
+      tableUsersapp.removeChild(tbody);
+      renderUserapp(change.doc);
     }
   })
 })
 
 // Click submit in add modal
-addModalcForm2.addEventListener('submit', e => {
+addModalappForm2.addEventListener('submit', e => {
   e.preventDefault();
-  db.collection('Criteria').add({
-    Name: addModalcForm2.Name.value,
-    Rate: addModalcForm2.Rate.value,
-    Description: addModalcForm2.Description.value,
+  db.collection('Applications').add({
+    firstname:addModalappForm3.firstname.value,
+    lastname :addModalappForm3.lastname.value,
+    email :addModalappForm3.email.value,
+    Nationality :addModalappForm3.Nationality.value,
+    StudentNr :addModalappForm3.StudentNr.value,
+    ContestantNo :addModalappForm3.ContestantNo.value,
   });
-  modalWrapperc.classList.remove('modal-showc');
+  modalWrapperapp.classList.remove('modal-showapp');
 });
 
 // Click submit in edit modal
-editModalcForm2.addEventListener('submit', e => {
+editModalappForm3.addEventListener('submit', e => {
   e.preventDefault();
-  db.collection('Criteria').doc(idc).update({
-    Name: editModalcForm2.Name.value,
-    Rate: editModalcForm2.Rate.value,
-    Description: editModalcForm2.Description.value,
+  db.collection('Criteria').doc(idapp).update({
+    firstname:editModalappForm3.firstname.value,
+    lastname :editModalappForm3.lastname.value,
+    email :editModalappForm3.email.value,
+    Nationality :editModalappForm3.Nationality.value,
+    StudentNr :editModalappForm3.StudentNr.value,
+    ContestantNo :editModalappForm3.ContestantNo.value,
   });
-  editModalc.classList.remove('modal-showc');
+  editModalapp.classList.remove('modal-showapp');
   
 });
